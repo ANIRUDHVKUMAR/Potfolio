283e92483b8957976b10bfb3844c585c9d5278df
ANIRUDH V KUMAR <118814650+ANIRUDHVKUMAR@users.noreply.github.com>
2024-02-09 11:48:08 +0530

    Update script.js

--- script.js
+++ script.js
@@ -66,4 +66,51 @@ document.addEventListener("DOMContentLoaded", function () {
   window.addEventListener("scroll", function() {
     header.classList.toggle("sticky", window.scrollY > 50);
   });
+
   
+
+
+
+  let menuIcon = document.querySelector("#menu-icon");
+  let navlist = document.querySelector(".navlist");
+
+  menuIcon.onclick = ()=>{
+    menuIcon.classList.toggle("bx-x");
+    navlist.classList.toggle("open");
+  }
+
+  menuIcon.onscroll = ()=>{
+    menuIcon.classList.remove("bx-x");
+    navlist.classList.remove("open");
+  }
+
+
+
+
+
+
+
+  const observer = new IntersectionObserver((entries)=>{
+    entries.forEach((entry)=>{
+      if(entry.isIntersecting){
+        entry.target.classList.add("show-items");
+      }
+      else{
+        entry.target.classList.remove("show-items");
+      }
+  });
+  });
+
+const scrollScale = document.querySelectorAll(".scroll-scale");
+scrollScale.forEach((el)=>observer.observe(el));
+
+const scrollBottom = document.querySelectorAll(".scroll-bottom");
+scrollBottom.forEach((el)=>observer.observe(el));
+
+const scrollTop = document.querySelectorAll(".scroll-top");
+scrollTop.forEach((el)=>observer.observe(el));
+
+
+
+
+
